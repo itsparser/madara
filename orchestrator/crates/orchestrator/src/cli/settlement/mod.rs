cd5bcdb41649b3640e3e55caa2c1dc75eb4e3eb5
itsparser <itsparser@gmail.com>
2025-04-27 11:55:08 +0530

    Resloved the couple of comments

--- orchestrator/crates/orchestrator/src/cli/settlement/mod.rs
+++ orchestrator/crates/orchestrator/src/cli/settlement/mod.rs
@@ -1,8 +1,2 @@
 pub mod ethereum;
 pub mod starknet;
-
-// #[derive(Clone, Debug)]
-// pub enum SettlementValidatedArgs {
-//     Ethereum(EthereumSettlementValidatedArgs),
-//     Starknet(StarknetSettlementValidatedArgs),
-// }
